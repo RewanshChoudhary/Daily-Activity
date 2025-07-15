f828e34ca41925b12701c36dca1fb365dee046e7
RewanshChoudhary <rewanshchoudhary57@gmail.com>
2025-07-16 00:02:01 +0530

    Working on CLIs in golang

---
+++ go-bench/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"os"
+	
+)
+
+type inputFile struct {
+	filepath  string
+	separator string
+	pretty    bool
+}
+
+func main() {
+
+}
+func getFileData() (inputFile, error) {
+	if len(os.Args) < 2 {
+		return inputFile{},errors.New("A filepath is required");
+
+
+
+
+	}
+	//Defining default values for separator and pretty value 
+	//Preparing three args 1: paraname 2: default value 3: short desc 
+
+	separator:=flag.String("separator","comma","column-separator")
+	pretty :=flag.Bool("pretty",false ,"A pretty json conversion")
+
+	flag.Parse()
+	filepath:=flag.Arg(0)
+
+
+	if (*separator!="comma" || *separator!="semicolon"){
+		return inputFile{},errors.New("The separator value was not valid")
+	}
+  
+
+
+   //if neither of the above things weren't caught then we send the input data 
+   
+	return inputFile{filepath,*separator,*pretty},nil
+
+
+
+
+}
